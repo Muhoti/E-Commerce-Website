1c9500e7e97855b8c1aa9497c6305bd16e05fd31
Strong Muhoti <strongmuhoti@gmail.com>
2023-04-24 15:51:18 +0300

    product detail page and shop page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+let MainImg = document.getElementById("MainImg");
+let smallImg = document.getElementsByClassName('small-img');
 
 if (bar) {
     bar.addEventListener('click', ()=>{
@@ -12,4 +14,17 @@ if(close) {
     close.addEventListener('click', ()=>{
         nav.classList.remove('active');
     });
+}
+
+smallImg[0].onclick = function() {
+    MainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function() {
+    MainImg.src = smallImg[0].src;
+}
+smallImg[2].onclick = function() {
+    MainImg.src = smallImg[0].src;
+}
+smallImg[3].onclick = function() {
+    MainImg.src = smallImg[0].src;
 }
